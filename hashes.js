3de192e8ac6490cec28c947dbab805c5b4ca0bf0
Daniel Huigens <d.huigens@gmail.com>
2015-04-20 15:17:37 +0200

    Add www.airbornos.com

--- hashes.js
+++ hashes.js
@@ -1,2 +1,11 @@
 module.exports = {
+	"www.airbornos.com": {
+		"37:A9:BB:AB:2C:CE:CE:37:C4:A5:76:00:5F:AA:22:4B:BF:19:70:13:3E:42:03:03:0C:BB:AA:C1:71:B5:62:85": {
+			"/": "sha256-dcba4e429355d826cdf010fc92e8841bdf69174f9520cfa04734cf34fda1e63b",
+			"/sjcl.js": "sha256-8da5110651396b572452674f4ee14c7498a0cb8fe6c2676e91bc5973d7a204fa",
+			"/login.js": "sha256-f44c8b5de348f5c8338c153bfe5b3163f2f174e955662ec8eccb05cce4abae39",
+			"/bootstrap.js": "sha256-7e599d0d0025cf770c58e81386c74938970bbc9d231748b5ce316da06cf9cc1a",
+			"/bootstrap.css": "sha256-c169430579d7bb5d7206674f5943b8d1055c87d6abe849e2d535b73c690d8d54",
+		},
+	},
 };
